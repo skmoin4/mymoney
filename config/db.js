fdf152956c1c9184e15edf9a24052e4951b5ab12
skmoin4 <smoin4425@gmail.com>
2025-10-14 14:54:47 +0530

    update

--- config/db.js
+++ config/db.js
@@ -1,12 +1,41 @@
+// import mysql from 'mysql2/promise';
+
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST || "localhost",
+//   user: process.env.DB_USER || "root",
+//   password: process.env.DB_PASSWORD || "",
+//   database: process.env.DB_NAME || "mymoney",
+//   port: process.env.DB_PORT || 3306,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+//   decimalNumbers: true
+// });
+
+// export async function query(sql, params = []) {
+//   try {
+//     const [rows] = await pool.execute(sql, params);
+//     return rows;
+//   } catch (error) {
+//     console.error("Database query error:", error);
+//     throw error;
+//   }
+// }
+
+// export function getPool() {
+//   return pool;
+// }
+
+
 import mysql from 'mysql2/promise';
 
-
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "mymoney",
-  port: process.env.DB_PORT || 3306,
+  host: "shortline.proxy.rlwy.net",
+  user: "root",
+  password: "UzkNQbiofsskGdnFflYVuEevkzKbTxVH",
+  database: "railway",
+  port: 59203,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
